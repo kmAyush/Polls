c90d763c6aedbf55423a6a588bc2e9322c4c2b86
kmAyush <kmayush27399@gmail.com>
2018-10-11 11:31:55 +0530

    on completing part 3

--- myProject/Polls/urls.py
+++ myProject/Polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'polls'
 urlpatterns=[
     path('', views.index, name='index'),
+    path('<int:question_id>//',views.details,name='detail'),
+    path('<int:question_id>/results/',views.results,name='result'),
+    path('<int:question_id>/vote/',views.vote,name='vote'),
 ]
--- myProject/Polls/views.py
+++ myProject/Polls/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Polls INDEX.")
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    context={	
+    	'latest_question_list':latest_question_list
+    	} 
+    return render(request,'polls/index.html',context)
+
+def details(request,question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request,question_id):
+    return HttpResponse("This is result of question %s." %question_id)
+
+def vote(request,question_id):
+    return HttpResponse("Here one can vote for question %s." %question_id)
