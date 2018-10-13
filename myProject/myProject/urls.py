6e2e0dc7d703904d84d10f7de797a924eadb1024
kmAyush <kmayush27399@gmail.com>
2018-10-13 11:54:47 +0530

    On completing Poll apps tutorials

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
 
+admin.site.site_header = "Polls"
+admin.site.site_title = 'Polls'
 urlpatterns = [
     path('polls/',include('polls.urls')),
+    path('',include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
