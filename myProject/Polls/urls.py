c0d478a935d4fdbff55ea73c173febfa21459431
kmAyush <kmayush27399@gmail.com>
2018-10-19 08:33:12 +0530

    Added Links for Forms, Edit and Deletion

--- myProject/Polls/urls.py
+++ myProject/Polls/urls.py
@@ -6,4 +6,9 @@ urlpatterns=[
     path('<int:pk>/',views.DetailView.as_view(),name='detail'),
     path('<int:pk>/results/',views.ResultsView.as_view(),name='results'),
     path('<int:question_id>/vote/',views.vote,name='vote'),
+    path('resultsALL/', views.ResultsallView.as_view(), name='resultsall'),
+    path('add/',views.questionadd,name='question-add'),
+    path('<int:question_id>/addChoice/',views.choiceadd,name='choiceadd'),
+    path('<int:question_id>/delete/',views.questiondelete,name='questiondelete'),
+    path('<int:question_id>/edit/',views.questionedit,name='questionedit')
 ]
