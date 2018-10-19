20332f5ff8faa74ef268dc7046abc6b8d8dafa0f
kmAyush <kmayush27399@gmail.com>
2018-10-19 08:21:30 +0530

    Added attributes 'null=True' and some minor changes

--- myProject/Polls/models.py
+++ myProject/Polls/models.py
@@ -2,10 +2,11 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 class Question(models.Model):
-    question_text= models.CharField(max_length=200)
-    pub_date= models.DateTimeField('date published')
+    question_text= models.CharField(max_length=500)
+    pub_date= models.DateTimeField('date published',null=True)
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
@@ -14,6 +15,9 @@ class Question(models.Model):
     was_published_recently.admin_order_field='pub_date'
     was_published_recently.boolean=True
     was_published_recently.short_description='Published recently?'    
+
+    def get_absolute_url(self):
+        return reverse('polls:detail', kwargs={'pk':self.pk})
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
